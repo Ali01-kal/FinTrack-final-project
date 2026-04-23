e87935b860cc56cf8f33a329ae05fe9f8165b598
Ali01-kal <ali.kalybek@mail.ru>
2026-04-23 21:36:57 +0500

    feat: initial commit

---
+++ lib/domain/entities/transaction_entity.dart
@@ -0,0 +1,13 @@
+class TransactionEntity {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  TransactionEntity({
+    required this.id, 
+    required this.title, 
+    required this.amount, 
+    required this.date
+  });
+}
